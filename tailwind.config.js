6044084f625340dc7e03061cd03f4fe9009b90d0
Vincasmen <vincasmen13@gmail.com>
2023-11-26 21:54:50 +0100

    Basic structure for the project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      xs: "425px",
+      ...defaultTheme.screens,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
